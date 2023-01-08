7cfe10204ec4bb51e108f7343ccf874c44e2f836
wangxiaodong <v-xiaodwang@microsoft.com>
2023-01-08 12:42:55 +0800

    a

--- main.py
+++ main.py
@@ -144,4 +144,5 @@ for idx in tqdm(range(len(sample_list))):
                                 videos_poses, video_basename, config.get('original_h'), config.get('original_w'), border=border, depth=depth, normal_canvas=normal_canvas, all_canvas=all_canvas,
                                 mean_loc_depth=mean_loc_depth)
         except Exception as e:
+            print(e)
             print('skip error...')
